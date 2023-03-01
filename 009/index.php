c8f08d40bc9d34438d35c764b02973b99e17bd2e
Vytautas Valutis <dnc908@gmail.com>
2023-03-01 11:34:08 +0200

    0301

---
+++ 009/index.php
@@ -0,0 +1,17 @@
+<?php
+
+// echo '<br>';
+// $jonas = rand(10, 20);
+// $petras = rand(5, 25);
+
+// if ($jonas > $petras) {
+//     echo "<h1> jonas laimi $jonas : $petras</h1>";
+// } elseif ($jonas < $petras) {
+//     echo "<h1> petras laimi $petras : $jonas</h1>";
+// } else {
+//     echo "<h1> niekas $petras : $jonas</h1>";
+// }
+
+for ($i=0; $i < 5; $i++) { 
+    echo " Nr .$i. <br>";
+}
