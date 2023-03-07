cb9940374a6958df9481a30a462f743e6f744bcc
Vytautas Valutis <dnc908@gmail.com>
2023-03-07 13:42:07 +0200

    HW502

--- 012/index.php
+++ 012/index.php
@@ -35,3 +35,25 @@ $arg = [3,3,3,3];
 $rez = fun(...$arg);
 echo "<br> $rez ";
 
+
+function fun3() {
+    
+    static $c = 0;
+    return ++$c;
+};
+
+$rez = fun3();
+echo "<br> $rez ";
+
+$rez = fun3();
+echo "<br> $rez ";
+
+$rez = fun3();
+echo "<br> $rez ";
+
+
+$fun4 = function() {
+    echo '<h2 style="color: blue";'
+};
+
+
