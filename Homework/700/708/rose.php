0eed81e1f3633309d07a1af2ec227ec94e07c850
Vytautas Valutis <dnc908@gmail.com>
2023-03-10 14:44:11 +0200

    HW708

---
+++ Homework/700/708/rose.php
@@ -0,0 +1,19 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] == 'GET') {
+    header('Location: http://localhost/phpProject/Homework/700/708/pink.php');
+    die;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="background-color: crimson">
+
+</body>
+</html>
