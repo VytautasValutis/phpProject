80747d7337f7c13bd8453bafe1b0a1000ac3740d
Vytautas Valutis <dnc908@gmail.com>
2023-03-10 16:09:52 +0200

    HW709

--- Homework/700/hw709.php
+++ Homework/700/hw709.php
@@ -1,9 +1,17 @@
 <?php
-foreach(range('A',chr(rand(67,75))) as $k) {
-    $arr[] = $k;
-};
-$color = '#fff';
-
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $color = '#000';
+    $colorb = '#fff';
+    $arr = [];
+    $rez = count($_POST);
+} else {
+    foreach(range('A',chr(rand(67,75))) as $k) {
+        $arr[] = $k;
+    };
+    $color = '#fff';
+    $colorb = '#000';
+    $rez = -1;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,13 +21,18 @@ $color = '#fff';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body style="background-color: black">
+<body style="background-color: <?= $colorb ?>">
     <form action="" method="post">
         <?php foreach($arr as $v) :?>
     <input type="checkbox" id="check<?= $v ?>" name="check<?= $v ?>" value="<?= $v ?>">
 <label style="color:<?= $color ?>" for="check<?= $v ?>"> <?= $v ?></label><br>
 <?php endforeach ?>
+<?php if($rez >= 0) : ?>
+        <h1> Pasirinkta: <?= $rez ?> </h1>
+        <?php else : ?>
         <button type="submit">CLICK ME</button> 
+        <?php endif ?>
+
     </form>
 
 </body>
