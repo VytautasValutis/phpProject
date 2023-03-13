df8e9dec5a1991ea4a0b09d2b72b02fdc4465a23
Vytautas Valutis <dnc908@gmail.com>
2023-03-13 16:00:30 +0200

    Class part 2 0313

---
+++ 015/forest/index.php
@@ -0,0 +1,14 @@
+<?php define('ENTER', true) ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forest for members</title>
+</head>
+<body>
+    <?php require(__DIR__ . '/../menu.php') ?>
+    <h1 style="color: red;">Welcome to dark forest</h1>
+</body>
+</html>
---
+++ 015/menu.php
@@ -0,0 +1,10 @@
+<?php
+defined('ENTER') || die('no entry');
+
+
+?>
+
+<h2 style="color: blue;">MENU</h2>
+<a href="http://localhost/phpProject/015/">Home</a>
+<a href="http://localhost/phpProject/015/forest/">Dark forest</a>
+<a href="http://localhost/phpProject/015/login/">Login</a>
